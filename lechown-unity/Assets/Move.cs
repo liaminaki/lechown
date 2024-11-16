f84925cda326e849ee0c0a13c729509053ea24ea
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2024-11-16 22:27:57 +0800

    Init game logic

---
+++ lechown-unity/Assets/Move.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	// Movement keys (customizable in inspector)
+	public KeyCode upKey;
+	public KeyCode downKey;
+	public KeyCode rightKey;
+	public KeyCode leftKey;
+
+	// Movement Speed
+	public float speed = 16;
+
+	// Wall Prefab
+	public GameObject wallPrefab;
+
+	// Current Wall
+	Collider2D wall;
+
+	// Last Wall's End
+	Vector2 lastWallEnd;
+
+	// Previous input
+	private KeyCode prevKey;
+
+	// Use this for initialization
+	void Start () {
+		// Initial Movement Direction
+		GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * speed;
+		spawnWall ();
+		prevKey = upKey;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Check for key presses
+		if (Input.GetKeyDown (upKey) && prevKey != downKey) {
+			GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * speed;
+			spawnWall ();
+			prevKey = upKey;
+		} else if (Input.GetKeyDown (downKey) && prevKey != upKey) {
+			GetComponent<Rigidbody2D>().linearVelocity = -Vector2.up * speed;
+			spawnWall ();
+			prevKey = downKey;
+		} else if (Input.GetKeyDown (rightKey) && prevKey != leftKey) {
+			GetComponent<Rigidbody2D>().linearVelocity = Vector2.right * speed;
+			spawnWall ();
+			prevKey = rightKey;
+		} else if (Input.GetKeyDown (leftKey) && prevKey != rightKey) {
+			GetComponent<Rigidbody2D>().linearVelocity = -Vector2.right * speed;
+			spawnWall ();
+			prevKey = leftKey;
+		}
+
+		fitColliderBetween (wall, lastWallEnd, transform.position);
+	}
+
+	void spawnWall() {
+		// Save last wall's position
+		lastWallEnd = transform.position;
+
+		// Spawn a new Lightwall
+		GameObject g = (GameObject)Instantiate (wallPrefab, transform.position, Quaternion.identity);
+		wall = g.GetComponent<Collider2D>();
+	}
+
+	void fitColliderBetween(Collider2D co, Vector2 a, Vector2 b) {
+		// Calculate the Center Position
+		co.transform.position = a + (b - a) * 0.5f;
+
+		// Scale it (horizontally or vertically)
+		float dist = Vector2.Distance (a, b);
+		if (a.x != b.x)
+			co.transform.localScale = new Vector2 (dist + 1, 1);
+		else
+			co.transform.localScale = new Vector2 (1, dist + 1);
+	}
+
+	void OnTriggerEnter2D(Collider2D co) {
+		if (co != wall) {
+			print ("Player lost:" + name);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ lechown-unity/README.md
@@ -0,0 +1,2 @@
+# unity-2d-tron-lightcycles
+Following tutorial at  http://noobtuts.com/unity/2d-tron-lightcycles-game 2D Tron Light-Cycles using unity
