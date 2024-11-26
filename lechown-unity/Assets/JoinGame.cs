a042ac499eb94b4731477477d3d0c1336f41d0e1
Dave Noceda <deibeunoceda26@gmail.com>
2024-11-26 17:48:12 +0800

    Add JoinGame script

---
+++ lechown-unity/Assets/JoinGame.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class JoinGame : MonoBehaviour
+{
+
+    [SerializeField] private InputField ipInputField;
+
+    private void Awake()
+    {
+        string ipAddress = ipInputField.text;
+        Network.Singleton.GetComponent<UnityTransport>().SetConnectionData(
+            ipAddress,      //The IP address entered by the player
+            7777            //Port number
+        );
+
+        NetworkManager.Singleton.StartClient();
+    }
+
+}
